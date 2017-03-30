6572699957a218a02a27841bb3e8b9ed5c0fe327
Stepan Chernovskiy <chernovsky.stepan@yandex.ru>
2017-03-30 19:49:58 +0700

    *sounds of John Cena*

---
+++ src/main/java/com/qoobico/remindme/server/controller/ReminderController.java
@@ -0,0 +1,40 @@
+package com.qoobico.remindme.server.controller;
+
+import com.qoobico.remindme.server.entity.Remind;
+import com.qoobico.remindme.server.service.ReminderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class ReminderController {
+
+    @Autowired
+    private ReminderService service;
+
+    @RequestMapping(value = "/reminders", method = RequestMethod.GET)
+    @ResponseBody
+    public List<Remind> getAllReminders() {
+        return service.getAll();
+    }
+
+    @RequestMapping(value = "/reminders/{id}", method = RequestMethod.GET)
+    @ResponseBody
+    public Remind getReminder(@PathVariable("id") long remindID) {
+        return service.getByID(remindID);
+    }
+
+    @RequestMapping(value = "/reminders", method = RequestMethod.POST)
+    @ResponseBody
+    public Remind saveRemider(@RequestBody Remind remind) {
+        return service.save(remind);
+    }
+
+    @RequestMapping(value = "/reminders/{id}", method = RequestMethod.DELETE)
+    @ResponseBody
+    public void delete(@PathVariable long id) {
+        service.remove(id);
+    }
+
+}
